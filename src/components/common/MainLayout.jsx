f4f07c52ea5f3bd1ebd3b8f026a920d25473b4f8
ramkachhwaha <ramkachhwaha062@gmail.com>
2025-12-26 17:41:00 +0530

    refactor MainLayout component structure

--- src/components/common/MainLayout.jsx
+++ src/components/common/MainLayout.jsx
@@ -13,6 +13,7 @@ export default function MainLayout() {
 
       {/* Main Content */}
       <main className="grow w-full">
+
         <div className="min-h-[calc(100vh-100px)]">
           <Outlet />
         </div>
